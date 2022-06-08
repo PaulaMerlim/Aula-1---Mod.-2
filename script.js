a02ea708fdc959ae7ba1a0f28a1581922ca850b2
PaulaMerlim <paula.merlim.77@gmail.com>
2022-06-08 12:59:56 -0300

    inserindo aprendizado aula 1 mod 2

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome ="Meu nome é Ana Paula Merlim "
+let numero = 1977
+let anoAtual = 2022
+let resultadoAno =(anoAtual - numero)
+console.log(nome)
+console.log("Eu tenho ",resultadoAno)
+let quartaFeira = true
+console.log(quartaFeira)
+console.log (typeof quartaFeira)
