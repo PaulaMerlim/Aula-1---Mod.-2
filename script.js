d14f3baad4bb7b8531168e210a5df57cee1c974e
PaulaMerlim <paula.merlim.77@gmail.com>
2022-06-10 11:17:04 -0300

    adicionando informação que faltava

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let anoAtual = 2022
 let resultadoAno =(anoAtual - numero)
 console.log(nome)
 console.log("Eu tenho ",resultadoAno)
+console.log ("Estou mexendo no console!")
 let quartaFeira = true
 console.log(quartaFeira)
 console.log (typeof quartaFeira)
